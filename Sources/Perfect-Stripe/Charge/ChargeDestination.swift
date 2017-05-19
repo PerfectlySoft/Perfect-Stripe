a1745a1fc5b0b0d39f3d904cd808f1dd8e0e8af3
Jonathan Guthrie <jono@perfect.org>
2017-05-19 12:57:45 -0400

    StripeChargeDestination class

--- Sources/Perfect-Stripe/Charge/ChargeDestination.swift
+++ Sources/Perfect-Stripe/Charge/ChargeDestination.swift
@@ -6,4 +6,20 @@
 //
 //
 
-import Foundation
+public class StripeChargeDestination {
+	// Used only for "Connect" Feature
+
+	///  ID of an existing, connected stripe account
+	public var account: String = ""
+
+	/// The amount to transfer to the destination account without creating an Application Fee. Cannot be combined with the application_fee parameter. Must be equal to or lesser than the charge amount
+	public var amount: Int = 0
+
+	public func asData() -> [String: Any] {
+		var d = [String: Any]()
+		d["account"] = account
+		d["amount"] = amount
+		return d
+	}
+	
+}
