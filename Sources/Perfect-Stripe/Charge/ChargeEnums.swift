c0ff4a253a09bae14720e221b7ea0450a18cc90b
Jonathan Guthrie <jono@perfect.org>
2017-05-19 12:57:16 -0400

    add own enums file

--- Sources/Perfect-Stripe/Charge/ChargeEnums.swift
+++ Sources/Perfect-Stripe/Charge/ChargeEnums.swift
@@ -6,4 +6,29 @@
 //
 //
 
-import Foundation
+public enum StripeChargeRefundReason: String {
+	case duplicate, fraudulent, requested_by_customer
+}
+
+public enum StripeChargeRefundStatus: String {
+	case pending, succeeded, failed, cancelled
+}
+
+public enum StripeChargeStatus: String {
+	case succeeded, pending, failed
+}
+
+public enum StripeChargeVerification: String {
+
+	/// The CVC, Zip or Address provided is correct
+	case pass,
+
+	/// The CVC, Zip or Address provided is incorrect
+	fail,
+
+	/// The customer's bank did not check the CVC, Zip or Address provided.
+	unavailable,
+
+	/// The CVC, Zip or Address was provided but has not yet been checked. Checks are performed once a card is attached to a Customer object, or when a Charge is created
+	unchecked
+}
