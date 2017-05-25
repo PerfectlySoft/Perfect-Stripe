cc517279bbc9767df276c840f48a91dfb3f4e613
Jonathan Guthrie <jono@perfect.org>
2017-05-25 16:12:00 -0400

    subscription stub files

---
+++ Sources/PerfectStripe/SubscriptionItems/SubscriptionItem.swift
@@ -0,0 +1,11 @@
+//
+//  SubscriptionItem.swift
+//  PerfectStripe
+//
+//  Created by Jonathan Guthrie on 2017-05-25.
+//
+//
+
+public class StripeSubscriptionItem {
+
+}
---
+++ Sources/PerfectStripe/Subscriptions/Subscription.swift
@@ -0,0 +1,11 @@
+//
+//  Sunscription.swift
+//  PerfectStripe
+//
+//  Created by Jonathan Guthrie on 2017-05-25.
+//
+//
+
+public class StripeSubscription {
+
+}
