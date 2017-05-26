0338555ba97a3a344087771d1876b00fd4fd42e4
Jonathan Guthrie <jono@perfect.org>
2017-05-26 11:09:08 -0400

    add parse and parseArray

--- Sources/PerfectStripe/SubscriptionItems/SubscriptionItem.swift
+++ Sources/PerfectStripe/SubscriptionItems/SubscriptionItem.swift
@@ -6,6 +6,8 @@
 //
 //
 
+import PerfectLib
+
 ///	Subscription items allow you to create customer subscriptions with more than one plan, 
 /// making it easy to represent complex billing relationships.
 public class StripeSubscriptionItem {
@@ -21,4 +23,36 @@ public class StripeSubscriptionItem {
 
 	///	The quantity of the plan to which the customer should be subscribed.
 	public var  quantity = 0
+
+
+
+
+	func parse(_ obj: [String: Any]) {
+
+		if let o = obj["id"], !(o is PerfectLib.JSONConvertibleNull) {
+			id = o as? String ?? ""
+		}
+		if let o = obj["created"], o is Int {
+			created = o as? Int ?? 0
+		}
+		if let o = obj["quantity"], o is Int {
+			quantity = o as? Int ?? 0
+		}
+		if let o = obj["plan"], !(o is PerfectLib.JSONConvertibleNull) {
+			plan.parse(o as? [String:Any] ?? [String:Any]())
+		}
+
+	}
+
+
+	func parseArray(_ o: [[String: Any]]) -> [StripeSubscriptionItem] {
+		var out = [StripeSubscriptionItem]()
+		o.forEach{
+			data in
+			let this = StripeSubscriptionItem()
+			this.parse(data)
+			out.append(this)
+		}
+		return out
+	}
 }
