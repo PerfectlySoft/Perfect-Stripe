9382820a09bc4af08a466dfff3584dafe8506f75
Jonathan Guthrie <jono@perfect.org>
2017-05-26 11:03:15 -0400

    base subscription item function

--- Sources/PerfectStripe/SubscriptionItems/SubscriptionItem.swift
+++ Sources/PerfectStripe/SubscriptionItems/SubscriptionItem.swift
@@ -6,6 +6,19 @@
 //
 //
 
+///	Subscription items allow you to create customer subscriptions with more than one plan, 
+/// making it easy to represent complex billing relationships.
 public class StripeSubscriptionItem {
 
+	/// Unique identifier for the object.
+	public var id = ""
+
+	///	Time at which the object was created. Measured in seconds since the Unix epoch.
+	public var created = 0
+
+	///	Hash describing the plan the customer is subscribed to.
+	public var plan: StripePlan = StripePlan()
+
+	///	The quantity of the plan to which the customer should be subscribed.
+	public var  quantity = 0
 }
