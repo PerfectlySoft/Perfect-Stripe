a324af585396cfaffeb0dbf48a3ee9b073efb6bf
Jonathan Guthrie <jono@perfect.org>
2017-05-26 11:02:37 -0400

    get func

--- Sources/PerfectStripe/Plans/PlanGet.swift
+++ Sources/PerfectStripe/Plans/PlanGet.swift
@@ -6,4 +6,17 @@
 //
 //
 
-import Foundation
+extension StripePlan {
+	/// Retrieves the plan with the given ID.
+	public func get(_ id: String) {
+		// execute request
+		let (response, code) = Stripe.makeRequest(.get, "/plans/\(id)")
+
+		if code != 200 {
+			print("StripeCharge.get Error: \(StripeHTTPErrorCode.fromCode(code))")
+		} else {
+			parse(response)
+		}
+	}
+
+}
