d474b9eb99d19f2dc4b1b4cc41712c7c84e7a5ee
Jonathan Guthrie <jono@guthrie.net.nz>
2018-04-03 01:56:13 -0400

    Charge Create

--- Sources/PerfectStripe/Methods/ChargeCreate.swift
+++ Sources/PerfectStripe/Methods/ChargeCreate.swift
@@ -6,6 +6,8 @@
 //
 
 import PerfectHTTP
+import PerfectCURL
+import codableRequest
 
 extension Stripe {
 	/// To charge a credit card or other payment source, you create a Charge object.
@@ -16,26 +18,28 @@ extension Stripe {
 	public static func chargeCreate(_ charge: Charge) throws -> Charge {
 
 		var params = [String: Any]()
-		guard charge.amount > 0 else {
+		guard charge.amount ?? 0 > 0 else {
 			throw StripeInputError.invalidInput(description: "Charged amount must be greater than zero.")
 		}
-		guard !charge.currency.isEmpty else {
+		guard let currency = charge.currency, !currency.isEmpty else {
 			throw StripeInputError.invalidInput(description: "Charged currency must not be empty.")
 		}
-		guard !charge.source.isEmpty, !charge.customer.isEmpty else {
+		let source = charge.source ?? ""
+		let customer = charge.customer ?? ""
+		if source.isEmpty, customer.isEmpty {
 			throw StripeInputError.invalidInput(description: "One of either 'source' or 'customer' must be used.")
 		}
 
 		params["amount"] = charge.amount
 		params["currency"] = charge.currency
 		// Connect only
-		if charge.application_fee > 0 {
+		if charge.application_fee ?? 0 > 0 {
 			params["application_fee"] = charge.application_fee
 		}
 		if charge.captured == false {
 			params["captured"] = false
 		}
-		if !charge.description.isEmpty {
+		if let description = charge.description, !description.isEmpty {
 			params["description"] = charge.description
 		}
 		// Connect only
@@ -43,35 +47,40 @@ extension Stripe {
 			params["destination"] = charge.destination
 		}
 		// Connect only
-		if !charge.transfer_group.isEmpty {
+		if let transfer_group = charge.transfer_group, !transfer_group.isEmpty {
 			params["transfer_group"] = charge.transfer_group
 		}
 		// Connect only
-		if !charge.on_behalf_of.isEmpty {
+		if let on_behalf_of = charge.on_behalf_of, !on_behalf_of.isEmpty {
 			params["on_behalf_of"] = charge.on_behalf_of
 		}
-		if !charge.metadata.isEmpty {
+		if charge.metadata != nil {
 			params["metadata"] = charge.metadata
 		}
-		if !charge.receipt_email.isEmpty {
+		if let receipt_email = charge.receipt_email, !receipt_email.isEmpty {
 			params["receipt_email"] = charge.receipt_email
 		}
 		if charge.shipping != nil {
 			params["shipping"] = charge.shipping
 		}
-		if !charge.customer.isEmpty {
+		if !customer.isEmpty {
 			params["customer"] = charge.customer
 		} else {
 			params["source"] = charge.source
 		}
-		if !charge.statement_descriptor.isEmpty {
+		if let statement_descriptor = charge.statement_descriptor, !statement_descriptor.isEmpty {
 			params["statement_descriptor"] = charge.statement_descriptor
 		}
 
 
 		// execute request
-		let response = try Stripe.makeRequest(.post, "/charges", json: params)
-		return try response.bodyJSON(Charge.self)
+		do {
+			let response: Charge = try CodableRequest.request(.post, "\(Stripe.server)/charges", to: Charge.self, error: ErrorResponse.self, params: params, encoding: "form", bearerToken: Stripe.apiKey)
+			return response
+		} catch {
+			throw error
+		}
+
 	}
 }
 
