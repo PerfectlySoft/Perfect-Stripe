2aae2a27e6b7fc79f04a1eadfbdc60476a99fa68
Jonathan Guthrie <jono@guthrie.net.nz>
2018-04-04 16:37:27 -0400

    create card now operational

--- Sources/PerfectStripe/Methods/CardCreate.swift
+++ Sources/PerfectStripe/Methods/CardCreate.swift
@@ -8,6 +8,8 @@
 import PerfectHTTP
 import PerfectCURL
 import codableRequest
+import PerfectLib
+import Foundation
 
 extension Stripe {
 	/// When you create a new credit card, you must specify a customer or recipient on which to create it.
@@ -16,7 +18,7 @@ extension Stripe {
 	/// To change the default, you should either update the customer to have a new default_source, or update the recipient to have a new default_card.
 	/// https://stripe.com/docs/api/curl#create_card
 	/// This first method is the simplest: It takes a custoer id, source, and an optional metadata dictionary and creates a card object
-	public static func cardCreate(customer: String, source: String, metadata: [String: String]?) throws -> Card {
+	public static func cardCreateSource(customer: String, source: String, metadata: [String: String]?) throws -> Card {
 
 		var params:[String: Any] = ["source": source]
 		if metadata != nil {
@@ -24,7 +26,7 @@ extension Stripe {
 		}
 
 		do {
-			let response: Card = try CodableRequest.request(.post, "\(Stripe.server)/customers/\(customer)/sources", to: Card.self, error: ErrorResponse.self, params: params, bearerToken: Stripe.apiKey)
+			let response: Card = try CodableRequest.request(.post, "\(Stripe.server)/customers/\(customer)/sources", to: Card.self, error: ErrorResponse.self, json: params, encoding: "form", bearerToken: Stripe.apiKey)
 			return response
 		} catch {
 			throw error
@@ -36,65 +38,67 @@ extension Stripe {
 
 
 	public static func cardCreate(customer: String, card: Card) throws -> Card {
+//		let cardparams = try JSONSerialization.jsonObject(with: try JSONEncoder().encode(card), options: []) as? [String : Any] ?? [String: Any]()
 
-		var params = [String: Any]()
-//		guard card.amount > 0 else {
-//			throw StripeInputError.invalidInput(description: "Charged amount must be greater than zero.")
-//		}
-//		guard let currency = card.currency, !currency.isEmpty else {
-//			throw StripeInputError.invalidInput(description: "Charged currency must not be empty.")
-//		}
-//		let source = card.source ?? ""
-//		let customer = card.customer ?? ""
-//		if source.isEmpty, customer.isEmpty {
-//			throw StripeInputError.invalidInput(description: "One of either 'source' or 'customer' must be used.")
-//		}
+		guard card.exp_month != nil else {
+			throw StripeInputError.invalidInput(description: "A exp_month value must be supplied.")
+		}
+		guard card.exp_year != nil else {
+			throw StripeInputError.invalidInput(description: "A exp_year value must be supplied.")
+		}
+		guard card.number != nil else {
+			throw StripeInputError.invalidInput(description: "A card number value must be supplied.")
+		}
+		guard card.currency != nil else {
+			throw StripeInputError.invalidInput(description: "A currency value must be supplied.")
+		}
 
-//		params["amount"] = charge.amount
-//		params["currency"] = charge.currency
-//		// Connect only
-//		if charge.application_fee > 0 {
-//			params["application_fee"] = charge.application_fee
-//		}
-//		if charge.captured == false {
-//			params["captured"] = false
-//		}
-//		if let description = charge.description, !description.isEmpty {
-//			params["description"] = charge.description
-//		}
-//		// Connect only
-//		if charge.destination != nil {
-//			params["destination"] = charge.destination
-//		}
-//		// Connect only
-//		if let transfer_group = charge.transfer_group, !transfer_group.isEmpty {
-//			params["transfer_group"] = charge.transfer_group
-//		}
-//		// Connect only
-//		if let on_behalf_of = charge.on_behalf_of, !on_behalf_of.isEmpty {
-//			params["on_behalf_of"] = charge.on_behalf_of
-//		}
-//		if charge.metadata != nil {
-//			params["metadata"] = charge.metadata
-//		}
-//		if let receipt_email = charge.receipt_email, !receipt_email.isEmpty {
-//			params["receipt_email"] = charge.receipt_email
-//		}
-//		if charge.shipping != nil {
-//			params["shipping"] = charge.shipping
-//		}
-//		if !customer.isEmpty {
-//			params["customer"] = charge.customer
-//		} else {
-//			params["source"] = charge.source
-//		}
-//		if let statement_descriptor = charge.statement_descriptor, !statement_descriptor.isEmpty {
-//			params["statement_descriptor"] = charge.statement_descriptor
-//		}
+//		var params = [String: Any]()
+
+		var cardparams: [String: Any] = ["source[object]": "card", "source[exp_month]": card.exp_month ?? 0, "source[exp_year]": card.exp_year ?? 0, "source[number]": card.number ?? ""]
+		if card.address_city != nil {
+			cardparams["source[address_city]"] = card.address_city
+		}
+		if card.address_country != nil {
+			cardparams["source[address_country]"] = card.address_country
+		}
+		if card.address_line1 != nil {
+			cardparams["source[address_line1]"] = card.address_line1
+		}
+		if card.address_line2 != nil {
+			cardparams["source[address_line2]"] = card.address_line2
+		}
+		if card.address_state != nil {
+			cardparams["source[address_state]"] = card.address_state
+		}
+		if card.address_zip != nil {
+			cardparams["source[address_zip]"] = card.address_zip
+		}
+		if card.currency != nil {
+			cardparams["source[currency]"] = card.currency
+		}
+		if card.cvc != nil {
+			cardparams["source[cvc]"] = card.cvc
+		}
+		if card.default_for_currency != nil {
+			cardparams["source[default_for_currency]"] = card.default_for_currency
+		}
+		if card.metadata != nil {
+			cardparams["source[metadata]"] = card.metadata
+		}
+		if card.name != nil {
+			cardparams["source[name]"] = card.name
+		}
 
 
+
+//		let params:[String: Any] = ["source": cardparams]
+//		if metadata != nil {
+//			params["metadata"] = metadata
+//		}
+//		let x = try params.jsonEncodedString()
 		do {
-			let response: Card = try CodableRequest.request(.post, "\(Stripe.server)/customers", to: Card.self, error: ErrorResponse.self, params: params, bearerToken: Stripe.apiKey)
+			let response: Card = try CodableRequest.request(.post, "\(Stripe.server)/customers/\(customer)/sources", to: Card.self, error: ErrorResponse.self, params: cardparams, encoding: "form", bearerToken: Stripe.apiKey)
 			return response
 		} catch {
 			throw error
