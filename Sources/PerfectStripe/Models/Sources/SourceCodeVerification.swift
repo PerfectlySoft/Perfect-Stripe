2c964d845a2c9bb1ad993f93fb50b4dd41436a36
Jonathan Guthrie <jono@guthrie.net.nz>
2018-04-04 16:46:26 -0400

    SourceCodeVerification struct

--- Sources/PerfectStripe/Models/Sources/SourceCodeVerification.swift
+++ Sources/PerfectStripe/Models/Sources/SourceCodeVerification.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension Stripe {
+
+	public struct SourceCodeVerification: Codable {
+		/// The number of attempts remaining to authenticate the source object with a verification code.
+		public var attempts_remaining: Int?
+
+		/// The status of the code verification, either pending (awaiting verification, attempts_remaining should be greater than 0), succeeded (successful verification) or failed (failed verification, cannot be verified anymore as attempts_remaining should be 0).
+		public var status: sourceVerificationStatus?
+
+	}
+}
