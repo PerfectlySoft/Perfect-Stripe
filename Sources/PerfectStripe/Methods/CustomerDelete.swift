49f4f3f1dc1613d889d4b2be95d0faa29257d262
Jonathan Guthrie <jono@guthrie.net.nz>
2018-04-05 12:32:03 -0400

    adding customer delete method

--- Sources/PerfectStripe/Methods/CustomerDelete.swift
+++ Sources/PerfectStripe/Methods/CustomerDelete.swift
@@ -5,4 +5,27 @@
 //  Created by Jonathan Guthrie on 2018-04-05.
 //
 
-import Foundation
+import PerfectHTTP
+import codableRequest
+
+extension Stripe {
+
+	// container for response
+	private struct CustomerDeleteContainer: Codable {
+		public var deleted: Bool?
+		public var id: String?
+	}
+
+	/// Deletes a customer object
+	public static func customerDelete(_ id: String) throws -> String? {
+
+		// execute request
+		do {
+			let response: CustomerDeleteContainer = try CodableRequest.request(.delete, "\(Stripe.server)/customers/\(id)", to: CustomerDeleteContainer.self, error: ErrorResponse.self, bearerToken: Stripe.apiKey)
+			return response.id
+		} catch {
+			throw error
+		}
+
+	}
+}
