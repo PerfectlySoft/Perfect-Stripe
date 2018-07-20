2d3a04e727b7b73377dddf26667ce9a8f6a7c017
Fatih Nayebi <f.nayebi@gmail.com>
2018-07-20 17:07:53 -0400

    Followed the standard model definition in PerfectStripe

--- Sources/PerfectStripe/Models/Keys/EphemeralKey.swift
+++ Sources/PerfectStripe/Models/Keys/EphemeralKey.swift
@@ -7,22 +7,24 @@
 
 import Foundation
 
-public struct StripeEphemeralKey: Codable {
-    public var id: String
-    public var object: String
-    public var associatedObjects: [[String : String]]?
-    public var created: Date?
-    public var expires: Date?
-    public var livemode: Bool
-    public var secret: String?
-    
-    public enum CodingKeys: String, CodingKey {
-        case id
-        case object
-        case associatedObjects = "associated_objects"
-        case created
-        case expires
-        case livemode
-        case secret
-    }
+extension Stripe {
+	public struct EphemeralKey: Codable {
+		public var id: String
+		public var object: String
+		public var associatedObjects: [[String : String]]?
+		public var created: Date?
+		public var expires: Date?
+		public var livemode: Bool
+		public var secret: String?
+		
+		public enum CodingKeys: String, CodingKey {
+			case id
+			case object
+			case associatedObjects = "associated_objects"
+			case created
+			case expires
+			case livemode
+			case secret
+		}
+	}
 }
