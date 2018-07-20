1334ebcacf3034c93fbeb6f1c6888bc2fa8e48ba
Fatih Nayebi <f.nayebi@gmail.com>
2018-07-20 16:52:23 -0400

    Ephemeral Key GET is updated to send the Stripe-Version to CodableRequest

--- Sources/PerfectStripe/Methods/EphemeralKeyGet.swift
+++ Sources/PerfectStripe/Methods/EphemeralKeyGet.swift
@@ -7,21 +7,22 @@
 
 import PerfectHTTP
 import PerfectCURL
-import codableRequest
 import PerfectLib
 import Foundation
+import codableRequest
 
 extension Stripe {
-    
-    public static func ephemeralKeyGet(customer: String, apiVersion: String) throws -> EphemeralKey {
-        
-        let params: [String: Any] = ["customer": customer, "api_version": apiVersion]
-        
-        do {
-            let response = try CodableRequest.request(.post, "\(Stripe.server)/ephemeral_keys", to: EphemeralKey.self, error: ErrorResponse.self, params: params, encoding: "form", bearerToken: Stripe.apiKey)
-            return response
-        } catch {
-            throw error
-        }
-    }
+	
+	public static func ephemeralKeyGet(customer: String, apiVersion: String) throws -> EphemeralKey {
+		
+		let params = ["customer": customer]
+		let headers = ["Stripe-Version": apiVersion]
+		
+		do {
+			let response = try CodableRequest.request(.post, "\(Stripe.server)/ephemeral_keys", to: EphemeralKey.self, error: ErrorResponse.self, params: params, encoding: "form", bearerToken: Stripe.apiKey, headers: headers)
+			return response
+		} catch {
+			throw error
+		}
+	}
 }
